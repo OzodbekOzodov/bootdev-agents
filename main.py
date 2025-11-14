865e35c9a47daa30a00ec88bde0d26bd128fc2e2
OzodbekOzodov <o.ozodov97@gmail.com>
2025-11-14 14:16:35 +0400

    prompt as a function

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from google.genai import types
-
-
+import sys
 api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
+def main(text:str):
+    response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = text
 )
-print(response.text)
-# Accessing the specific counts
-usage = response.usage_metadata
-prompt_tokens = usage.prompt_token_count
-response_tokens = usage.candidates_token_count 
+    print(response.text)
+    # Accessing the specific counts
+    usage = response.usage_metadata
+    prompt_tokens = usage.prompt_token_count
+    response_tokens = usage.candidates_token_count 
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
 
-print(f"Tokens in Prompt: {prompt_tokens}")
-print(f"Tokens in Response: {response_tokens}")
+if __name__ == "__main__":
+    text = sys.argv[1]
+    main(text)
